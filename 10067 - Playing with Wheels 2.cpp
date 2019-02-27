c656bad73a2a0759fc9796f241799cab8057603a
Tanver Ahmed Likhon <39611778+TanverLikhon@users.noreply.github.com>
2019-02-28 01:00:51 +0600

    ..

---
+++ 10067 - Playing with Wheels 2.cpp
@@ -0,0 +1,226 @@
+/*Never Give up*/
+/*
+Problem:http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=1008
+Verdict :AC
+Time:0.040 Rank: 87 :)
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define sf(a)          scanf("%d",&a)
+#define sff(a,b)         scanf("%d%d",&a,&b)
+#define sfff(a,b,c)         scanf("%d%d%d",&a,&b,&c)
+#define sffff(a,b,c,d)         scanf("%d%d%d%d",&a,&b,&c,&d)
+#define pf         printf
+#define ll           long long
+#define ull        unsigned long long
+#define pb        push_back
+#define v           vector
+#define l            list
+#define pi          pair
+#define mp         map
+#define nl         printf("\n")
+#define en        pf("Entered\n")
+#define MAX  100002
+int s1,s2,s3,s4;
+int dd1,dd2,dd3,dd4;
+int ck[10][10][10][10];
+int dest;
+struct cell
+{
+    int a,b,c,d;
+};
+int ps(int x)
+{
+    if(x<0)
+        return 9;
+    else if(x>9)
+        return 0;
+
+    return x;
+}
+
+int bfs()
+{
+    //en;
+    int dis[10][10][10][10];
+    bool visited [10][10][10][10];// cout<< visited[s1][s2][s3][s4]<<endl;
+    for(int i=0; i<10; i++)
+    {
+        for(int j=0; j<10; j++)
+        {
+            for(int k=0; k<10; k++)
+            {
+                for(int l=0; l<10; l++)
+                {
+                    visited[i][j][k][l]=false;
+                    dis[i][j][k][l]=-1;
+
+                }
+            }
+        }
+    }
+    cell u;
+    u.a=s1,u.b=s2,u.c=s3,u.d=s4;
+    dis[s1][s2][s3][s4]=0;
+    visited[s1][s2][s3][s4]=true;
+    queue<cell>q;
+    q.push(u);
+    while(!q.empty())
+    {
+        cell x=q.front();
+        q.pop();
+        cell tmp;
+        int aa,bb,cc,dd;
+        aa=x.a,bb=x.b,cc=x.c,dd=x.d;
+      if(aa==dd1&&bb==dd2&&cc==dd3&&dd==dd4)    return dis[dd1][dd2][dd3][dd4];
+
+        int a1=ps(aa+1),a2=ps(aa-1),b1=ps(bb+1),b2=ps(bb-1);
+        int c1=ps(cc+1),c2=ps(cc-1),d1=ps(dd+1),d2=ps(dd-1);
+        if(visited[aa][bb][cc][d1]==false)
+        {
+            visited[aa][bb][cc][d1]=true;
+            dis[aa][bb][cc][d1]= dis[aa][bb][cc][dd]+1;
+            tmp.a=aa;
+            tmp.b=bb;
+            tmp.c=cc;
+            tmp.d=d1;
+            //    if(makenumber(aa,bb,cc,d1)<=dest)
+
+            //  if(ck[makenumber(aa,bb,cc,d1)]!=1)
+            if(ck[aa][bb][cc][d1]!=1)
+                q.push(tmp);
+        }
+
+        if(visited[aa][bb][cc][d2]==false)
+        {
+            visited[aa][bb][cc][d2]=true;
+            dis[aa][bb][cc][d2]= dis[aa][bb][cc][dd]+1;
+            tmp.a=aa;
+            tmp.b=bb;
+            tmp.c=cc;
+            tmp.d=d2;
+            if(ck[aa][bb][cc][d2]!=1)
+                q.push(tmp);
+        }
+
+
+        if(visited[aa][bb][c1][dd]==false)
+        {
+            visited[aa][bb][c1][dd]=true;
+            dis[aa][bb][c1][dd]= dis[aa][bb][cc][dd]+1;
+            tmp.a=aa;
+            tmp.b=bb;
+            tmp.c=c1;
+            tmp.d=dd;
+            //              if(makenumber(aa,bb,c1,dd)<=dest)
+
+            if(ck[aa][bb][c1][dd]!=1)
+                q.push(tmp);
+        }
+
+        if(visited[aa][bb][c2][dd]==false)
+        {
+            visited[aa][bb][c2][dd]=true;
+            dis[aa][bb][c2][dd]= dis[aa][bb][cc][dd]+1;
+            tmp.a=aa;
+            tmp.b=bb;
+            tmp.c=c2;
+            tmp.d=dd;
+            //                 if(makenumber(aa,bb,c2,dd)<=dest)
+
+            if(ck[aa][bb][c2][dd]!=1)
+                q.push(tmp);
+        }
+
+
+
+        if(visited[aa][b1][cc][dd]==false)
+        {
+            visited[aa][b1][cc][dd]=true;
+            dis[aa][b1][cc][dd]= dis[aa][bb][cc][dd]+1;
+            tmp.a=aa;
+            tmp.b=b1;
+            tmp.c=cc;
+            tmp.d=dd;
+            //             if(makenumber(aa,b1,cc,dd)<=dest)
+
+            if(ck[aa][b1][cc][dd]!=1)
+                q.push(tmp);
+        }
+
+        if(visited[aa][b2][cc][dd]==false)
+        {
+            visited[aa][b2][cc][dd]=true;
+            dis[aa][b2][cc][dd]= dis[aa][bb][cc][dd]+1;
+            tmp.a=aa;
+            tmp.b=b2;
+            tmp.c=cc;
+            tmp.d=dd;
+
+            if(ck[aa][b2][cc][dd]!=1)
+                q.push(tmp);
+        }
+
+
+
+        if(visited[a1][bb][cc][dd]==false)
+        {
+            visited[a1][bb][cc][dd]=true;
+            dis[a1][bb][cc][dd]= dis[aa][bb][cc][dd]+1;
+            tmp.a=a1;
+            tmp.b=bb;
+            tmp.c=cc;
+            tmp.d=dd;
+            if(ck[a1][bb][cc][dd]!=1)
+                q.push(tmp);
+        }
+
+        if(visited[a2][bb][cc][dd]==false)
+        {
+            visited[a2][bb][cc][dd]=true;
+            dis[a2][bb][cc][dd]= dis[aa][bb][cc][dd]+1;
+            tmp.a=a2;
+            tmp.b=bb;
+            tmp.c=cc;
+            tmp.d=dd;
+            if(ck[a2][bb][cc][dd]!=1)
+                q.push(tmp);
+        }
+
+    }
+    return -1;
+}
+
+int main()
+{
+    int t,f;
+    sf(t);
+    while(t--)
+    {
+        sffff(s1,s2,s3,s4);
+        sffff(dd1,dd2,dd3,dd4);
+        for(int i=0; i<10; i++)
+        {
+            for(int j=0; j<10; j++)
+            {
+                for(int k=0; k<10; k++)
+                {
+                    for(int l=0; l<10; l++)
+                    {
+                        ck[i][j][k][l]=0;
+                    }
+                }
+            }
+        }
+        sf(f);
+        while(f--)
+        {
+            int x,y,z,zx;
+            sffff( x,y,z,zx);
+            ck[x][y][z][zx]=1;
+        }
+        pf("%d\n",bfs());
+
+    }
+    return 0;
+}
--- Untitled3.cpp
+++ Untitled3.cpp
@@ -1,89 +1,124 @@
+/*Never Give up*/
+/*
+Problem:
+Verdict :
+Time:
+*/
 #include<bits/stdc++.h>
 using namespace std;
-#define sf          scanf
+#define sf(a)          scanf("%d",&a)
+#define sff(a,b)         scanf("%d%d",&a,&b)
+#define sfff(a,b,c)         scanf("%d%d%d",&a,&b,&c)
 #define pf         printf
 #define ll           long long
 #define ull        unsigned long long
 #define pb        push_back
 #define v           vector
 #define l            list
-#define p          pair
-#define m         map
+#define pi          pair
+#define mp         map
 #define nl         printf("\n")
-#define white 1
-#define gray   2
-#define black 3
-char land[21][21];
-int w,h;
-int color[21][21];
-int ck;
-void dfsVisit(int x,int y)
+#define en        pf("Entered\n")
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+char adj[1000][1000];
+int n,m;
+int srcx,srcy,destx,desty;
+bool visited[1000][1000];
+int check1[1000][1000];
+int r1,c1,r0,c0;
+bool isvalid(int row, int col)
 {
+    return (row >= 0) && (row < n) &&
+           (col >= 0) && (col < m);
+}
+struct cell
+{
+    int x,y;
+    int x0,y0,x1,y1;
+};
+void bfs(int row,int col)
+{
+    r1=r0,c1=c0;
+    visited[row][col]=true;
+    queue<cell>q;
+    cell point;
+    point.x=row;
+    point.y=col;
+    point.x0=point.x1=r0;
+    point.y0=point.y1=c0;
+    q.push(point);
+    check1[row][col]=0;
+    while(!q.empty())
+    {
+        cell u;
+        u=q.front();
+        q.pop();
+        for(int i=0; i<4; i++)
+        {
+            row=u.x+dx[i];
+            col=u.y+dy[i];
+            if(isvalid(row,col)==true&&adj[row][col]!='*'&&visited[row][col]==false)
+            {
+                r1=u.x1,c1=u.y1;
+                r0=u.x0,c0=u.y0;
+                cout<<"---------------------"<<endl;
+                cout<<r1<<"   "<<c1<<endl;
+                cout<<r0<<"  "<<c0<<endl;
+                cout<<"current "<<endl;
+                cout<<row<<"  "<<col<<endl;
+                cout<<"---------------------"<<endl;
 
-    color[x][y]=gray;
-    int i=x,j=y;
-    if(land[i][j-1]=='.'&&color[i][j-1]==white&&i<h&&(j-1)<w)
-    {
-        ck++;
-        color[i][j-1]=gray;
-        dfsVisit(i,j-1);
-    }
-    if(land[i][j+1]=='.'&&color[i][j+1]==white&&i<h&&(j+1)<w)
-    {
-        ck++;
-        color[i][j+1]=gray;
-        dfsVisit(i,j+1);
-    }
-    if(land[i-1][j]=='.'&&color[i-1][j]==white&&(i-1)<h&&j<w)
-    {
-        ck++;
-        color[i-1][j]=gray;
-        dfsVisit(i-1,j);
-    }
+                visited[row][col]=true;
 
+                if(row!=r0&&col!=c0)
+                {
+                    check1[row][col]=check1[u.x][u.y]+1;
+                }
+                else
+                    check1[row][col]=check1[u.x][u.y];
 
-    if(land[i+1][j]=='.'&&color[i+1][j]==white&&(i+1)<h&&j<w)
-    {
-        ck++;
-        color[i+1][j]=gray;
-        dfsVisit(i+1,j);
+                cell tmp;
+                tmp.x=row,tmp.y=col;
+                tmp.x1=row,tmp.y1=col,tmp.x0=r1,tmp.y0=c1;
+                q.push(tmp);
+            }
+        }
+
     }
-    color[x][y]=black;
 
 }
-
 int main()
 {
-
-    int t;
-    sf("%d",&t);
-    int c=1;
-    while(t--)
+    sff(n,m);
+    for(int i=0; i<n; i++)
     {
-        sf("%d%d",&w,&h);
-        int x,y;
-        for(int i=0; i<h; i++)
+        for(int j=0; j<m; j++)
         {
-            string str;
-            cin>>str;
-            for(int j=0; j<w; j++)
-            {
-                land[i][j]=str[j];
-                color[i][j]=white;
-                if(str[j]=='@')
-                {
-                    x=i,y=j;
-                }
-            }
-
+            cin>>adj[i][j];
+            if(adj[i][j]=='S')
+                srcx=i,srcy=j,r0=i,c0=j;
+            else if(adj[i][j]=='T')
+                destx=i,desty=j;
         }
-//cout<<x<<"   "<<y<<endl;
-        dfsVisit(x,y);
-    pf("Case %d: %d\n",c,ck+1);
-    c++;
-        ck=0;
     }
-
+    check1[srcx][srcy]=0;
+    bfs(srcx,srcy);
+    /*   for(int i=0; i<n; i++)
+       {
+           for(int j=0; j<m; j++)
+           {
+               cout<<check1[i][j]<<" ";
+               // cout<<visited[i][j]<<" ";
+           }
+           nl;
+       }
+       cout<<check1[destx][desty]<<endl;*/
+    if(check1[destx][desty]<=2&&visited[destx][desty])
+        pf("YES\n");
+    else
+        pf("NO\n");
+    //cout<<check1[destx][desty]<<endl;
     return 0;
 }
 
